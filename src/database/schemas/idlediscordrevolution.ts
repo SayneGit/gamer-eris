c8c09b5e7959e2122639d86a145ae57a284a2c58
Skillz <gankstarsleague@gmail.com>
2020-04-11 11:38:12 -0400

    idle game schema base

--- src/database/schemas/idlediscordrevolution.ts
+++ src/database/schemas/idlediscordrevolution.ts
@@ -1,11 +1,9 @@
 import mongoose from 'mongoose'
 
 export default new mongoose.Schema({
-  guildID: { type: String, required: true },
-  memberID: { type: String, required: true },
-}).index({ memberID: 1, guildID: 1 })
+  userID: { type: String, required: true, index: true }
+})
 
 export interface GamerIdleDiscordRevolution extends mongoose.Document {
   userID: string
-  guildID: string
 }
