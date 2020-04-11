b6ab2f8a8f582c0063ec582a2b65f67e96ebf5a2
Skillz <gankstarsleague@gmail.com>
2020-04-11 11:53:34 -0400

    more schema base

--- src/database/schemas/idlediscordrevolution.ts
+++ src/database/schemas/idlediscordrevolution.ts
@@ -1,9 +1,13 @@
 import mongoose from 'mongoose'
 
 export default new mongoose.Schema({
-  userID: { type: String, required: true, index: true }
+  userID: { type: String, required: true, index: true },
+  currency: { type: Number, default: 10 },
+  prestigeMultiplier: { type: Number, default: 1 }
 })
 
 export interface GamerIdleDiscordRevolution extends mongoose.Document {
   userID: string
+  currency: number
+  prestigeMultiplier: number
 }
